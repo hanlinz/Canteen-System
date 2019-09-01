d040ea890a6e09b95fa328a64e1785b9d9dc399e
hanlinz <761323865@qq.com>
2019-09-01 20:13:23 +0800

    修改myrequest页面，增加type传参，

--- project/js/config.js
+++ project/js/config.js
@@ -1,2 +1,10 @@
 var baseURL = 'http://139.217.97.220:80'
 var userid = 'yum3';
+
+// 获取url参数的公共方法
+function getUrlParam(name) {//封装方法
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg); //匹配目标参数
+    if (r != null) return unescape(r[2]);
+    return null; //返回参数值
+}
